40741e52165b006eac82ee7be0aedf2e90ba327e
Oscar Carlsson <oscar@spindel.tax>
2026-01-10 10:43:11 +0100

    Import plain60

---
+++ keyboards/evyd13/plain60/keymaps/oscarcarlsson/keymap.c
@@ -0,0 +1,162 @@
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+enum _layer {
+  _MA,
+  _FN,
+  _LW,
+  _GM,
+  _GF
+};
+
+#define _______ KC_TRNS
+#define ___T___ KC_TRNS
+#define XXXXXXX KC_NO
+
+enum custom_keycodes {
+  CT_JIG = SAFE_RANGE,
+};
+
+#define CT_FSPC LT(_FN, KC_SPC)
+#define CT_FAPP LT(_LW, KC_APP)
+#define CT_BASE DF(_MA)
+#define CT_GAME DF(_GM)
+#define CT_GMFN MO(_GF)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[_MA] = LAYOUT(
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
+  KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,
+  KC_LCTL, KC_LGUI, KC_LALT,                        CT_FSPC,                       XXXXXXX, KC_RALT, CT_FAPP, XXXXXXX),
+
+[_FN] = LAYOUT(
+  QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_INS,
+  XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+  XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CT_JIG,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX,                            _______,                            XXXXXXX, XXXXXXX, _______, XXXXXXX),
+
+[_LW] = LAYOUT(
+  QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_PSCR,
+  XXXXXXX, KC_MPLY, KC_VOLU, KC_MSTP, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT, KC_PGDN, CT_GAME, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CT_JIG,
+  XXXXXXX, XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX,                            _______,                            XXXXXXX, XXXXXXX, _______, XXXXXXX),
+
+[_GM] = LAYOUT(
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
+  KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,
+  KC_LCTL, KC_LGUI, KC_LALT,                        KC_SPC,                        XXXXXXX, KC_RALT, CT_GMFN, XXXXXXX),
+
+[_GF] = LAYOUT(
+  CT_BASE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_PSCR,
+  XXXXXXX, KC_MPLY, KC_VOLU, KC_MSTP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CT_JIG,
+  XXXXXXX, XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX,                            _______,                            XXXXXXX, XXXXXXX, _______, XXXXXXX)
+};
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    static deferred_token token = INVALID_DEFERRED_TOKEN;
+    static report_mouse_t report = {0};
+
+    if (token) {
+      // If jiggler is currently running, stop when any key is pressed.
+      cancel_deferred_exec(token);
+      token = INVALID_DEFERRED_TOKEN;
+      report = (report_mouse_t){};  // Clear the mouse.
+      host_mouse_send(&report);
+    } else if (keycode == CT_JIG) {
+
+      uint32_t jiggler_callback(uint32_t trigger_time, void* cb_arg) {
+        // Deltas to move in a circle of radius 20 pixels over 32 frames.
+        static const int8_t deltas[32] = {
+          0, -1, -2, -2, -3, -3, -4, -4, -4, -4, -3, -3, -2, -2, -1, 0,
+          0, 1, 2, 2, 3, 3, 4, 4, 4, 4, 3, 3, 2, 2, 1, 0};
+        static uint8_t phase = 0;
+        // Get x delta from table and y delta by rotating a quarter cycle.
+        report.x = deltas[phase];
+        report.y = deltas[(phase + 8) & 31];
+        phase = (phase + 1) & 31;
+        host_mouse_send(&report);
+        return 16;  // Call the callback every 16 ms.
+      }
+
+      token = defer_exec(1, jiggler_callback, NULL);  // Schedule callback.
+    }
+  }
+
+  switch (keycode) {
+  case KC_BSPC: {
+    // https://getreuer.info/posts/keyboards/macros3/index.html
+    static uint16_t registered_key = KC_NO;
+    if (record->event.pressed) {  // On key press.
+      const uint8_t mods = get_mods();
+#ifndef NO_ACTION_ONESHOT
+      uint8_t shift_mods = (mods | get_oneshot_mods()) & MOD_MASK_SHIFT;
+#else
+      uint8_t shift_mods = mods & MOD_MASK_SHIFT;
+#endif  // NO_ACTION_ONESHOT
+      if (shift_mods) {  // At least one shift key is held.
+        registered_key = KC_DEL;
+        // If one shift is held, clear it from the mods. But if both
+        // shifts are held, leave as is to send Shift + Del.
+        if (shift_mods != MOD_MASK_SHIFT) {
+#ifndef NO_ACTION_ONESHOT
+          del_oneshot_mods(MOD_MASK_SHIFT);
+#endif  // NO_ACTION_ONESHOT
+          unregister_mods(MOD_MASK_SHIFT);
+        }
+      } else {
+        registered_key = KC_BSPC;
+      }
+
+      register_code(registered_key);
+      set_mods(mods);
+    } else {  // On key release.
+      unregister_code(registered_key);
+    }
+  } return false;
+
+  }
+  return true;
+}
+
+#if defined(OS_DETECTION_ENABLE)
+uint32_t custom_os_settings(uint32_t trigger_time, void *cb_arg) {
+  os_variant_t host = detected_host_os();
+  uint16_t retry_ms = 500;
+
+  if (host == OS_MACOS || host == OS_IOS) {
+    keymap_config.swap_lalt_lgui = true;
+    keymap_config.swap_ralt_rgui = true;
+    retry_ms = 0;
+  }
+
+  return retry_ms;
+}
+#endif
+
+
+void keyboard_post_init_user(void) {
+#if defined(DEFERRED_EXEC_ENABLE)
+  defer_exec(100, custom_os_settings, NULL);
+#endif
+#if defined(CONSOLE_ENABLE)
+  debug_enable = true;
+#endif
+}

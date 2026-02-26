e17827734cfd8d2802abf0719db2aef2cdb9e6ee
Oscar Carlsson <oscar@spindel.tax>
2026-02-26 14:34:58 +0100

    plain60: Add right control

--- keyboards/evyd13/plain60/keymaps/oscarcarlsson/keymap.c
+++ keyboards/evyd13/plain60/keymaps/oscarcarlsson/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
   KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,
-  KC_LCTL, KC_LGUI, KC_LALT,                        CT_FSPC,                       XXXXXXX, KC_RALT, CT_FAPP, XXXXXXX),
+  KC_LCTL, KC_LGUI, KC_LALT,                        CT_FSPC,                       XXXXXXX, KC_RALT, CT_FAPP, KC_RCTL),
 
 [_FN] = LAYOUT(
   QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_INS,
